9270fa81e4aaa6c1bb54ec56f7452881cd952fed
morgrynn <anthony21harvey@gmail.com>
2022-02-09 17:33:05 +0200

    Login

--- auth/src/routes/login.ts
+++ auth/src/routes/login.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/requestValidationError";
+
+
 
 const router = express.Router();
 
-router.post("/api/users/login", (req, res) => {
-  res.send("Hello There From Netflix!");
+router.post("/api/users/login", [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Must have valid password"),
+], (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 });
 
 export { router as loginRouter };
