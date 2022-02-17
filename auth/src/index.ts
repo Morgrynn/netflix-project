2e9e920e1163838160ada361723058b6509bedf1
morgrynn <anthony21harvey@gmail.com>
2022-02-17 09:21:45 +0200

    Auth: Code cleanup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,20 +1,19 @@
-import mongoose from "mongoose";
-import { app } from './app'
-
+import mongoose from 'mongoose';
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
+    throw new Error('JWT_KEY must be defined');
   }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-    console.log("Connected to MongoDB");
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Auth: Connected to MongoDB');
   } catch (err) {
-    console.error(err);
+    console.error('Error', err);
   }
 
   app.listen(5000, () => {
-    console.log("NF-Auth: Listening on port 5000");
+    console.log('NF-Auth: Listening on port 5000');
   });
 };
 
