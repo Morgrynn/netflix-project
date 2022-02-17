1d01b2bca5d8f8fdb42b293d24a523dc94843e31
morgrynn <anthony21harvey@gmail.com>
2022-02-17 09:48:30 +0200

    Client: navbar links

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,15 +1,29 @@
 import Link from 'next/link';
 
 export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Register', href: '/auth/register' },
+    !currentUser && { label: 'Login', href: '/auth/login' },
+    currentUser && { label: 'Logout', href: '/auth/logout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className='nav-item'>
+          <Link href={href}>
+            <a className='nav-link'>{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className='navbar navbar-light bg-light'>
       <Link href='/'>
         <a className='navbar-brand'>FitFlex</a>
       </Link>
       <div className='d-flex justify-content-end'>
-        <ul className='nav d-flex align-items-center'>
-          {currentUser ? 'Logout' : 'Login'}
-        </ul>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
       </div>
     </nav>
   );
