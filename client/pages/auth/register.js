716baf8da16319aa48ad4f886f18de883e740aa3
morgrynn <anthony21harvey@gmail.com>
2022-02-17 17:36:28 +0200

    Client: Register styles

--- client/pages/auth/register.js
+++ client/pages/auth/register.js
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/useRequest';
+import styles from '../../styles/Register.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default () => {
   const [email, setEmail] = useState('');
@@ -12,33 +15,91 @@ export default () => {
     onSuccess: () => Router.push('/'),
   });
 
+  const emailRef = useRef();
+  // const passwordRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+  // const handleFinish = async () => {
+  //   setPassword(passwordRef.current.value);
+  //   await doRequest();
+  // };
+
   const onSubmit = async (event) => {
     event.preventDefault();
     await doRequest();
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <h1>Register</h1>
-      <div className='form-group'>
-        <label>Email Address</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className='form-control'
-        />
+    <div className={styles.register}>
+      <div className={styles.top}>
+        <div className={styles.wrapper}>
+          <Image src='/logo.png' alt='logo' width='200' height='50' />
+          <Link href='/auth/login'>
+            <a className={styles.link}>Sign In</a>
+          </Link>
+        </div>
       </div>
-      <div className='form-group'>
-        <label>Password</label>
-        <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className='form-control'
-        />
+      <div className={styles.container}>
+        <h1 className={styles.headerone}>Unlimited workout shows.</h1>
+        <h2 className={styles.headertwo}>
+          Get in the best shape of your life.
+        </h2>
+        <p className={styles.p}>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className={styles.inputgroup}>
+            <input
+              className={styles.input}
+              type='email'
+              placeholder='email address'
+              ref={emailRef}
+            />
+            <button className={styles.registerButton} onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className={styles.inputgroup} onSubmit={onSubmit}>
+            <input
+              className={styles.input}
+              type='password'
+              placeholder='password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className={styles.startButton}>
+              Start
+            </button>
+            {errors}
+          </form>
+        )}
       </div>
-      {errors}
-      <button className='btn btn-primary'>Register</button>
-    </form>
+      {/* <form onSubmit={onSubmit}>
+        <h1>Register</h1>
+        <div className='form-group'>
+          <label>Email Address</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='form-control'
+            ref={emailRef}
+          />
+        </div>
+        <div className='form-group'>
+          <label>Password</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={styles.input}
+            ref={passwordRef}
+          />
+        </div>
+        {errors}
+        <button className='btn btn-primary'>Register</button>
+      </form> */}
+    </div>
   );
 };
